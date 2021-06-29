9933130e2e71babf55036f34e86df12251eaef4e
Toader <Cristian-Matei.Toader@ing.com>
2021-06-29 16:13:00 +0300

    moduile 3

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 
-
-
 let express = require('express');
 let path = require('path');
+let router = require('./src/router')
 
 const app = express()
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html')
+
+app.use('/', router)
+app.use(express.static(path.resolve(__dirname, 'uploads')))
 app.use('/*', (req, res) => res.sendFile(pathToIndex))
 
 module.exports = app
